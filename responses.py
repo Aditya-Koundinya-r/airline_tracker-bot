f0f2861ed21e2216973f24854230f42e2697a0c3
Aditya-Koundinya-r <100258776+Aditya-Koundinya-r@users.noreply.github.com>
2022-02-23 14:10:07 +0530

    Add files via upload

--- responses.py
+++ responses.py
@@ -4,17 +4,15 @@ def sampleResponse(input_text):
 
     userMessage = str(input_text).lower()
 
-    if userMessage in ("hello", "hi", "whatsup",):
-        return "Hey man! How is it going?"
+    if userMessage in ("hello", "hi", "start",):
+        return "HI, WELCOME"
 
-    if userMessage in ("who are you", "who are ?",):
-        return "I am your bot! hahaha "
 
-    if userMessage in ("time", "time?","what is the time?"):
+    if userMessage in ("time", "time?"):
         now = datetime.now()
-        dateTime = now.strftime("%d/%m/%y, %H:%M:%S")
+        dateTime = now.strftime("%d/%m/%y, %H:%M")
 
         return str(dateTime)
 
 
-    return "I don't understand you man. Why are you so complicated"
+    return "HI THIS IS A BOT"
